f130f73796613b4ead4a7cdf7c5599e8f15969e8
Abortkleshengeren <sales@scrapeitout.com>
2016-04-15 13:43:17 +0200

    Update client init

--- project/main.py
+++ project/main.py
@@ -6,6 +6,13 @@ import socket
 from threading import Thread, Lock
 from constants import *
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8",80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
 def main():
     UDP_PORT = 39500
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,25 +20,29 @@ def main():
     sock.bind(('', 39500))
     sock.settimeout(3)
     try:
-        msg = sock.recv(4096)
+        msg, master_addr = sock.recvfrom(4096)
         if (msg == 'master'):
             state = 'client'
     except (socket.timeout):
         state = 'master'
+    sock.close()
 
     if (state == 'master'):
-        print "Initiating master"
+        print "Initiating.."
+        my_ip = get_ip()
         master = Master()
-        print "Initiating server"
-        server = network.ThreadedTCPServer(('localhost',10001), network.ClientHandler)
+        server = network.ThreadedTCPServer((my_ip,10001), network.ClientHandler)
         server.master = master
         server.lock = Lock()
+        print "Ready to serve"
         server.serve_forever()
     else:
-        client = network.Client('localhost', 10001)
+        client = network.Client(master_addr, 10001)
         worker = network.Msg_receiver(client, client.connection)
         worker.setDaemon(True)
         worker.start()
+        lock = Lock()
+        elev = Elev(ELEV_MODE, lock)
         running = True
         while running:
             raw = raw_input()
